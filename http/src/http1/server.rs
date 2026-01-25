3288d02d80de020cf8092bae99a8c2dc5a408ae6
SamiC++ <samichentouf+git2@gmail.com>
2026-01-25 15:27:02 +0100

    add client read logic in http1

--- http/src/http1/server.rs
+++ http/src/http1/server.rs
@@ -1,19 +1,189 @@
-use crate::shared::{HttpType, Stream, server::HttpSocket};
+use std::collections::HashMap;
+use std::{pin::Pin};
 
-pub struct Http1Socket<S: Stream>{
-    pub net: S,
+use std::io;
+use tokio::io::{AsyncBufReadExt, AsyncReadExt, BufReader, ReadHalf, WriteHalf};
+use crate::shared::HttpMethod;
+use crate::shared::{HttpType, HttpVersion, ReadStream, Stream, WriteStream, server::{HttpClient, HttpSocket}};
+
+pub const H2C_UPGRADE: &'static [u8] = b"HTTP/1.1 101 Switching Protocols\r\nConnection: Upgrade\r\nUpgrade: h2c\r\n\r\n";
+pub const WS_UPGRADE: &'static [u8] = b"HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: ";
+
+
+
+#[derive(Debug)]
+pub struct Http1Socket<R: ReadStream, W: WriteStream>{
+    pub netr: BufReader<R>,
+    pub netw: W,
+
+    pub client: Http1Client,
+    pub line_buf: Vec<u8>,
 }
 
-impl<S:Stream> Http1Socket<S>{
-    pub fn new(net: S) -> Self{
-        Http1Socket { 
-            net,
-        }
+#[derive(Debug, Clone)]
+pub struct Http1Client{
+    pub valid: bool,
+
+    pub mpv_complete: bool,
+    pub head_complete: bool,
+    pub body_complete: bool,
+    
+    pub method: HttpMethod,
+    pub path: String,
+    pub version: HttpVersion,
+
+    pub headers: HashMap<String, Vec<String>>,
+    pub body: Vec<u8>,
+}
+
+
+impl<S:Stream> Http1Socket<ReadHalf<S>, WriteHalf<S>>{
+    pub fn new(net: S, bufsize: usize) -> Self{
+        let (netr, netw) = tokio::io::split(net);
+        let bufr = BufReader::with_capacity(bufsize, netr);
+        Self::with_split(bufr, netw)
     }
 }
 
-impl<S:Stream> HttpSocket for Http1Socket<S>{
+impl<R: ReadStream, W: WriteStream> Http1Socket<R, W>{
+    pub fn with_split(netr: BufReader<R>, netw: W) -> Self {
+        Self {
+            netr,
+            netw,
+            client: Default::default(),
+            line_buf: Vec::new(),
+        }
+    }
+
+    pub async fn read_client(&mut self) -> io::Result<&Http1Client>{
+        self.line_buf.clear();
+
+        if !self.client.valid {
+
+        }
+        else if !self.client.mpv_complete{
+            let _ = self.netr.read_until(b'\n', &mut self.line_buf).await?;
+
+            let fullstr = String::from_utf8_lossy(&self.line_buf);
+            let mpv: Vec<&str> = fullstr.splitn(3, ' ').collect();
+
+            if mpv.len() < 2{
+                self.client.valid = false;
+            }
+            else if mpv.len() == 2{
+                self.client.method = HttpMethod::Get;
+                self.client.path = mpv[1].to_owned();
+                self.client.version = HttpVersion::Http09;
+
+                self.client.head_complete = true;
+                self.client.body_complete = true;
+            }
+            else{
+                self.client.method = HttpMethod::from(mpv[0]);
+                self.client.path = mpv[1].to_owned();
+                self.client.version = 
+                if mpv[2].eq_ignore_ascii_case("http/1.0") { HttpVersion::Http10 }
+                else if mpv[2].eq_ignore_ascii_case("http/1.1") { HttpVersion::Http11 }
+                else { HttpVersion::Unknown(Some(mpv[2].to_owned())) };
+            }
+
+            self.client.mpv_complete = true;
+        }
+        else if !self.client.head_complete{
+            let _ = self.netr.read_until(b'\n', &mut self.line_buf).await?;
+            let fullstr = String::from_utf8_lossy(&self.line_buf);
+            let hv: Vec<&str> = fullstr.splitn(2, ':').map(|e|e.trim()).collect();
+
+            if fullstr.trim().is_empty(){
+                self.client.head_complete = true;
+            }
+            else if hv.len() == 1 {
+                self.client.valid = false;
+            }
+            else{
+                if let Some(hs) = self.client.headers.get_mut(&hv[0].to_ascii_lowercase()) { hs.push(hv[1].to_owned()); }
+                else { self.client.headers.insert(hv[0].to_ascii_lowercase(), vec![ hv[1].to_owned() ]); }
+            }
+        }
+        else if !self.client.body_complete{
+            if let Some(te) = self.client.headers.get("transfer-encoding") && te[0].contains("chunked") {
+                let _ = self.netr.read_until(b'\n', &mut self.line_buf).await?;
+                let string = String::from_utf8_lossy(&self.line_buf);
+                let len = match usize::from_str_radix(string.trim(), 16) { Ok(s) => s, Err(_) => 0, };
+
+                if len == 0{
+                    self.client.body_complete = true;
+                }
+                else{
+                    let ol = self.client.body.len();
+                    self.client.body.resize(self.client.body.len() + len + 1, 0);  // len + 1 allows LF seperated chunks (technically should be CRLF)
+                    self.netr.read_exact(&mut self.client.body[ol..]).await?;
+                }
+            }
+            else if let Some(cl) = self.client.headers.get("content-length") && let Ok(len) = cl[0].parse::<usize>(){
+                self.client.body.resize(len, 0);
+                self.netr.read_exact(&mut self.client.body).await?;
+                self.client.body_complete = true;
+            }
+            else{
+                self.client.body_complete = true;
+            }
+        }
+
+        Ok(&self.client)
+    }
+}
+
+impl<R: ReadStream, W: WriteStream> HttpSocket for Http1Socket<R, W>{
     fn get_type() -> HttpType {
         HttpType::Http1
+    }
+
+    fn get_client(&self) -> &dyn HttpClient {
+        &self.client
+    }
+    fn read_client(&'_ mut self) -> Pin<Box<dyn Future<Output = Result<&'_ dyn HttpClient, std::io::Error>> + '_>> {
+        Box::pin(async move {
+            self.read_client().await.and_then(|c| Ok(c as &dyn HttpClient))
+        })
+    }
+}
+
+
+impl HttpClient for Http1Client{
+    fn get_method(&self) -> &HttpMethod {
+        &self.method
+    }
+    fn get_path(&self) -> &str {
+        &self.path
+    }
+    fn get_version(&self) -> &HttpVersion {
+        &self.version
+    }
+
+    fn get_headers(&self) -> &HashMap<String, Vec<String>> {
+        &self.headers
+    }
+    fn get_body(&self) -> &[u8] {
+        &self.body
+    }
+}
+
+impl Default for Http1Client{
+    fn default() -> Self {
+        Self {
+            valid: true,
+
+            mpv_complete: false,
+            head_complete: false,
+            body_complete: false,
+
+            method: HttpMethod::Unknown(None),
+            path: String::new(),
+            version: HttpVersion::Unknown(None),
+
+            headers: HashMap::new(),
+            body: Vec::new(),
+        }
     }
 }
--- http/src/shared.rs
+++ http/src/shared.rs
@@ -4,8 +4,15 @@ use tokio::io::{AsyncRead, AsyncWrite};
 
 
 
-pub trait Stream: AsyncRead + AsyncWrite + Unpin + Send + Sync {}
-impl<A> Stream for A where A: AsyncRead + AsyncWrite + Unpin + Send + Sync {}
+pub trait ReadStream: AsyncRead + Unpin + Send + Sync {}
+impl<A> ReadStream for A where A: AsyncRead + Unpin + Send + Sync {}
+
+pub trait WriteStream: AsyncWrite + Unpin + Send + Sync {}
+impl<A> WriteStream for A where A: AsyncWrite + Unpin + Send + Sync {}
+
+pub trait Stream: ReadStream + WriteStream {}
+impl<A> Stream for A where A: ReadStream + WriteStream {}
+
 
 #[derive(Debug, Clone)]
 pub enum HttpType{
@@ -49,12 +56,70 @@ impl Display for HttpVersion{
     }
 }
 
+#[derive(Debug, Clone)]
+pub enum HttpMethod{
+    Unknown(Option<String>),
+
+    Get,
+    Head,
+    Post,
+    Put,
+    Delete,
+    Connect,
+    Options,
+    Trace,
+}
+impl Display for HttpMethod{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Unknown(s) => if let Some(v) = s { write!(f, "Unknown({})", v) } else { write!(f, "Unknown") },
+
+            Self::Get => write!(f, "GET"),
+            Self::Head => write!(f, "HEAD"),
+            Self::Post => write!(f, "POST"),
+            Self::Put => write!(f, "PUT"),
+            Self::Delete => write!(f, "DELETE"),
+            Self::Connect => write!(f, "CONNECT"),
+            Self::Options => write!(f, "OPTIONS"),
+            Self::Trace => write!(f, "TRACE"),
+        }
+    }
+}
+impl HttpMethod{
+    pub fn from(string: &str) -> Self{
+        if string.eq_ignore_ascii_case("get") { Self::Get }
+        else if string.eq_ignore_ascii_case("head") { Self::Head }
+        else if string.eq_ignore_ascii_case("post") { Self::Post }
+        else if string.eq_ignore_ascii_case("put") { Self::Put }
+        else if string.eq_ignore_ascii_case("delete") { Self::Delete }
+        else if string.eq_ignore_ascii_case("connect") { Self::Connect }
+        else if string.eq_ignore_ascii_case("options") { Self::Options }
+        else if string.eq_ignore_ascii_case("trace") { Self::Trace }
+        else { Self::Unknown(Some(string.to_owned())) }
+    }
+}
+
+
 pub mod server{
+    use std::{collections::HashMap, pin::Pin};
+
     use super::*;
 
-    pub trait HttpClient{}
+    pub trait HttpClient{
+        fn get_method<'_a>(&'_a self) -> &'_a HttpMethod;
+        fn get_path<'_a>(&'_a self) -> &'_a str;
+        fn get_version<'_a>(&'_a self) -> &'_a HttpVersion;
+
+        fn get_headers<'_a>(&'_a self) -> &'_a HashMap<String, Vec<String>>;
+        fn get_body<'_a>(&'_a self) -> &'_a [u8];
+    }
     pub trait HttpSocket{
         fn get_type() -> HttpType;
+
+        fn get_client<'_a>(&'_a self) -> &'_a dyn HttpClient;
+        fn read_client<'_a>(&'_a mut self) -> Pin<Box<dyn Future<Output = Result<&'_a dyn HttpClient, std::io::Error>> + '_a>>;
     }
+
+    pub type DynHttpSocket = Box<dyn HttpSocket>;
 }
 
