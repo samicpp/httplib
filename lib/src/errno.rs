e1db2c31d3aa7b26712da6459f5c7a935bc78f35
samic++ <sami.cplusplus0@gmail.com>
2026-02-22 19:51:29 +0100

    fix duplicate error code

--- lib/src/errno.rs
+++ lib/src/errno.rs
@@ -6,30 +6,6 @@ pub const ERROR: i32 = 0x100;
 pub const IO_ERROR: i32 = 0x200;
 
 
-pub fn errno_liberr(error: &LibError) -> i32 {
-    match error {
-        LibError::Io(io) => io.raw_os_error().unwrap_or(0) | IO_ERROR,
-        LibError::Huffman(_) => 0x101,
-        LibError::Hpack(_) => 0x102,
-
-        LibError::NotConnected => 0x102,
-        LibError::ConnectionClosed => 0x103,
-        LibError::StreamClosed => 0x104,
-        LibError::HeadersSent => 0x105,
-
-        LibError::Invalid => 0x106,
-        LibError::InvalidFrame => 0x107,
-        LibError::InvalidUpgrade => 0x108,
-        LibError::InvalidStream => 0x109,
-        LibError::InvalidString => 0x110,
-
-        LibError::NotAccepted => 0x111,
-        LibError::ResetStream => 0x112,
-        LibError::Goaway => 0x113,
-        LibError::ProtocolError => 0x114,
-    }
-}
-
 pub trait Errno {
     fn get_errno(&self) -> i32;
 }
@@ -41,21 +17,21 @@ impl Errno for LibError {
             Self::Huffman(_) => 0x101,
             Self::Hpack(_) => 0x102,
 
-            Self::NotConnected => 0x102,
-            Self::ConnectionClosed => 0x103,
-            Self::StreamClosed => 0x104,
-            Self::HeadersSent => 0x105,
+            Self::NotConnected => 0x103,
+            Self::ConnectionClosed => 0x104,
+            Self::StreamClosed => 0x105,
+            Self::HeadersSent => 0x106,
 
-            Self::Invalid => 0x106,
-            Self::InvalidFrame => 0x107,
-            Self::InvalidUpgrade => 0x108,
-            Self::InvalidStream => 0x109,
-            Self::InvalidString => 0x110,
+            Self::Invalid => 0x107,
+            Self::InvalidFrame => 0x108,
+            Self::InvalidUpgrade => 0x109,
+            Self::InvalidStream => 0x110,
+            Self::InvalidString => 0x111,
 
-            Self::NotAccepted => 0x111,
-            Self::ResetStream => 0x112,
-            Self::Goaway => 0x113,
-            Self::ProtocolError => 0x114,
+            Self::NotAccepted => 0x112,
+            Self::ResetStream => 0x113,
+            Self::Goaway => 0x114,
+            Self::ProtocolError => 0x115,
         }
     }
 }
